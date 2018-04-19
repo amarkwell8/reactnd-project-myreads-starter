a62fffecf7081699690289a7953e6062b091a50a
Markwell <markwella@AZMac-C02SY1KVGTFM.domain>
2018-04-18 23:46:11 -0700

    Switched from hardcoded data for books, to using the given BooksAPI

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -9,7 +9,7 @@ if (!token)
 
 const headers = {
   'Accept': 'application/json',
-  'Authorization': token
+  'Authorization': "whatevers"
 }
 
 export const get = (bookId) =>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,21 +2,21 @@ import React, {Component} from 'react'
 
 class ListBooks extends Component{
     render(){
-        const { books, shelfStatus } = this.props;
+        const { books, shelfStatus, onChangeShelf } = this.props;
         const currentShelf = this.props.shelfStatus;
         return(
       
                     <div className="bookshelf-books">
                         <ol className="books-grid">
                         {this.props.books.filter(function(book){
-                            return book.status === currentShelf;
+                            return book.shelf === currentShelf;
                         }).map((book) => (
                             <li key={book.title}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.coverUrl})` }}></div>
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                         <div className="book-shelf-changer">
-                                                <select value={book.status}>
+                                                <select defaultValue={book.shelf} onChange={() =>onChangeShelf(book)}>
                                                     <option value="none" disabled>Move to...</option>
                                                     <option value="currentlyReading">Currently Reading</option>
                                                     <option value="wantToRead">Want to Read</option>
@@ -26,7 +26,7 @@ class ListBooks extends Component{
                                         </div>
                                     </div>
                                     <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.author}</div>
+                                    <div className="book-authors">{book.authors}</div>
                                 </div>
                             </li>
                         ))}
